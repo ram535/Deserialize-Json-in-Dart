340ae6fd11d739d906e175e8341dc0a8a8da2062
last <ramses.aldama@gmail.com>
2020-01-12 02:47:37 +0100

    json simple nested structures alternative

--- bin/main.dart
+++ bin/main.dart
@@ -5,21 +5,30 @@ void main() async {
   String jsonString = await File('student.json').readAsString();
   final Map jsonResponse = json.decode(jsonString);
   Student student = Student.fromJson(jsonResponse);
-  print(student.topicScores);
+  print(student.score.art);
 }
 
 class Student {
   final String name;
-  final Map<String, int> topicScores;
+  final Score score;
 
-  Student({this.name, this.topicScores});
+  Student({this.name, this.score});
 
   factory Student.fromJson(Map<String, dynamic> parsedJson) {
-    var topicScoresFromJson = parsedJson['topicScores'];
-    print(topicScoresFromJson.runtimeType);
-    Map<String, int> topicScoresList =
-        Map<String, int>.from(topicScoresFromJson);
-    // Map<String, int> topicScoresList = topicScoresFromJson.cast<String, int>();
-    return Student(name: parsedJson['name'], topicScores: topicScoresList);
+    return Student(
+        name: parsedJson['name'], score: Score.fromJson(parsedJson['score']));
+  }
+}
+
+class Score {
+  double math;
+  double spanish;
+  double art;
+  Score({this.math, this.spanish, this.art});
+  factory Score.fromJson(Map<String, dynamic> json) {
+    print(json.runtimeType);
+    print(json['math'].runtimeType);
+    return Score(
+        math: json['math'], spanish: json['spanish'], art: json['art']);
   }
 }
