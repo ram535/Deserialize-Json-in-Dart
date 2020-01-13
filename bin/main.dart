c3e6fffb7aee2d491dc734ba7a8f855bde9f2579
last <ramses.aldama@gmail.com>
2020-01-13 07:23:00 +0100

    Use strong mode with nested structure in a list

--- bin/main.dart
+++ bin/main.dart
@@ -3,7 +3,8 @@ import 'dart:io' show File;
 
 void main() async {
   String jsonString = await File('school.json').readAsString();
-  final Map jsonResponse = json.decode(jsonString);
+  final Map<String, dynamic> jsonResponse =
+      json.decode(jsonString) as Map<String, dynamic>;
   School school = School.fromJson(jsonResponse);
   print(school.students[0].name);
 }
@@ -16,11 +17,14 @@ class School {
 
   factory School.fromJson(Map<String, dynamic> parsedJson) {
     print(parsedJson['students'].runtimeType); //List<dynamic>
-    var list = parsedJson['students']; // as List
-    // List<Student> studentList = list.map((i) => Student.fromJson(i)).toList();
-    List<Student> studentList =
-        list.map<Student>((i) => Student.fromJson(i)).toList();
-    return School(schoolName: parsedJson['schoolName'], students: studentList);
+    var list = parsedJson['students'] as List;
+    List<Student> studentList = list
+        .map((dynamic i) => Student.fromJson(i as Map<String, dynamic>))
+        .toList();
+    // List<Student> studentList =
+    //     list.map<Student>((i) => Student.fromJson(i)).toList();
+    return School(
+        schoolName: parsedJson['schoolName'] as String, students: studentList);
   }
 }
 
@@ -29,6 +33,8 @@ class Student {
   double score;
   Student({this.name, this.score});
   factory Student.fromJson(Map<String, dynamic> parsedJson) {
-    return Student(name: parsedJson['name'], score: parsedJson['score']);
+    return Student(
+        name: parsedJson['name'] as String,
+        score: parsedJson['score'] as double);
   }
 }
